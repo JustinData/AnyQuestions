21e405ead72e805a95b740a757279c5f6c0587b0
Nichol Alexander <nichol.alexander@gmail.com>
2013-11-10 16:27:09 -0500

    added paused and dismissed to questions table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131110031818) do
+ActiveRecord::Schema.define(version: 20131110211508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 20131110031818) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "paused",      default: false
+    t.boolean  "dismissed",   default: false
   end
 
   create_table "upvotes", force: true do |t|
