314758da4920be8b7993e4e1b4c395d6e9bd7cde
JustinGA <justinkemmerer@gmail.com>
2013-11-11 12:17:50 -0500

    users can create accounts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,22 @@ class UsersController < ApplicationController
 		@users = User.all
 	end
 
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+
+		if @user.save
+      		redirect_to users_path #user_path(@user)
+    	else
+      		render :new
+    	end
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:email, :password, :password_confirmation, :name)
+	end
 end
