5987887900b5e060f180f7210d45a1058a229821
JustinGA <justinkemmerer@gmail.com>
2013-11-11 11:30:42 -0500

    configured underscore, created users model and controller, and seeds, users index page loads

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.destroy_all
+User.create(name: 'Justin', email: 'icacnt@gmail.com', password_digest: 'decaff')
+User.create(name: 'Joe', email: 'joe@shmoe.com', password_digest: 'decaff')
+User.create(name: 'John', email: 'john@doe.com', password_digest: 'decaff')
